ee24aa4e6ea498c9a2e364ffe656a63835db37de
jerh56 <jerh56@gmail.com>
2022-04-02 10:03:41 -0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const router = Router();
 router.get('/', (req, res) => {    
     res.json(
         {
-            "Title": "Hola mundo usando rutas!"
+            "Title": "Marey Holding"
         }
     );
 })
